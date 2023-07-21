9f973939d4a40bdf9bae15f7188ac0379eb017ea
YunHaaaa <qlxqlrt2012@gmail.com>
2023-07-21 22:30:21 +0900

    Feat: Add Header

---
+++ src/components/util.ts
@@ -0,0 +1,18 @@
+import { ChartPieIcon, CursorArrowRaysIcon, FingerPrintIcon, SquaresPlusIcon, ArrowPathIcon, PlayCircleIcon, PhoneIcon } from '@heroicons/react/24/outline';
+
+export const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+];
+
+export const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+];
+
+export function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(' ');
+}
