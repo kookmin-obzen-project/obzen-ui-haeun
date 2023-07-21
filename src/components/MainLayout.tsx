8c63ef0c299b393a3559dda5a3efd5d9fbae3e75
YunHaaaa <qlxqlrt2012@gmail.com>
2023-07-21 23:54:11 +0900

    Feat: Check isMobile

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,27 +1,41 @@
+import { useState, useEffect } from 'react';
 import MobileHeader from './MobileHeader';
 import DesktopHeader from './DesktopHeader';
 import Body from './Body';
 
 const MainLayout = () => {
-    // Define a breakpoint value that determines when the screen is considered mobile
-    const mobileBreakpoint = 768; // You can adjust this value based on your design and requirements
-  
-    // Get the current window width
-    const windowWidth = window.innerWidth;
-  
-    // Check if the window width is below the mobile breakpoint to determine if it's a mobile screen
-    const isMobile = windowWidth < mobileBreakpoint;
-    
+  // Define a breakpoint value that determines when the screen is considered mobile
+  const mobileBreakpoint = 768; // You can adjust this value based on your design and requirements
+
+  // Get the current window width
+  const getWindowWidth = () => window.innerWidth;
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+  // Check if the window width is below the mobile breakpoint to determine if it's a mobile screen
+  const isMobile = windowWidth < mobileBreakpoint;
+
+  useEffect(() => {
+    // Function to update window width state when the window is resized
+    const handleResize = () => {
+      setWindowWidth(getWindowWidth());
+    };
+
+    // Add event listener to window resize event
+    window.addEventListener('resize', handleResize);
+    console.log('isMobile:', isMobile);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+
   return (
     <>
       {isMobile ? <MobileHeader isOpen={false} /> : <DesktopHeader />}
       {/* Rest of your layout content... */}
-
-      <div className="py-8 text-center">
-        <h1 className="text-4xl font-bold">Main Layout Content</h1>
-        <p className="mt-4 text-lg text-gray-600">This is the content of MainLayout.</p>
-      </div>
-      <Body /> {/* Body 컴포넌트를 렌더링합니다. */}
+      <Body />
     </>
   );
 };
