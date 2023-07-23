f8cb5478882e8fc7dbe96e4fa99b9aa45a563b88
YunHaaaa <qlxqlrt2012@gmail.com>
2023-07-23 16:47:02 +0900

    Feat: Body test

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,98 +1,55 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
-import { useState } from 'react';
-
-const features = [
-  {
-    name: 'Push to deploy.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
-  },
-]
-
 export default function Body() {
-  const [selectedView, setSelectedView] = useState('view1');
-
   return (
-    <div className="flex flex-grow">
-    {/* 왼쪽 영역 */}
-    <div className="w-2/8 bg-indigo-50 p-6">
-      <h2 className="text-base font-semibold leading-7 text-indigo-600">Choose a view</h2>
-      <div className="mt-4">
-          <label className="inline-flex items-center space-x-2 cursor-pointer">
-            <input
-              type="radio"
-              className="form-radio h-4 w-4 text-indigo-600"
-              value="view1"
-              checked={selectedView === 'view1'}
-              onChange={() => setSelectedView('view1')}
-            />
-            <span className="text-gray-900">View 1</span>
-          </label>
-          <label className="inline-flex items-center space-x-2 cursor-pointer">
-            <input
-              type="radio"
-              className="form-radio h-4 w-4 text-indigo-600"
-              value="view2"
-              checked={selectedView === 'view2'}
-              onChange={() => setSelectedView('view2')}
-            />
-            <span className="text-gray-900">View 2</span>
-          </label>
-          <label className="inline-flex items-center space-x-2 cursor-pointer">
-            <input
-              type="radio"
-              className="form-radio h-4 w-4 text-indigo-600"
-              value="view3"
-              checked={selectedView === 'view3'}
-              onChange={() => setSelectedView('view3')}
-            />
-            <span className="text-gray-900">View 3</span>
-          </label>
+
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
+                    </svg>
+                    Push to deploy.
+                  </dt>
+                  <dd className="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                </div>
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                    </svg>
+                    SSL certificates.
+                  </dt>
+                  <dd className="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
+                </div>
+                <div className="relative pl-9">
+                  <dt className="inline font-semibold text-gray-900">
+                    <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
+                      <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
+                    </svg>
+                    Database backups.
+                  </dt>
+                  <dd className="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <img
+          src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+          alt="Product screenshot"
+          className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          width="2432"
+          height="1442"
+        />
         </div>
       </div>
-
-      {/* 오른쪽 영역 */}
-      <div className="w-6/8 flex flex-col justify-center items-center">
-        {selectedView === 'view1' && (
-          
-          <>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">View 1</h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              This is the content of View 1.
-            </p>
-          </>
-        )}
-        {selectedView === 'view2' && (
-          <>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">View 2</h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              This is the content of View 2.
-            </p>
-          </>
-        )}
-        {selectedView === 'view3' && (
-          <>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">View 3</h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              This is the content of View 3.
-            </p>
-          </>
-        )}
-        <button className="mt-4 px-6 py-3 rounded-lg bg-indigo-600 text-white text-base font-semibold">
-          버튼
-        </button>
-      </div>
-    </div>
-  );
+    </div>  
+    );
 }
